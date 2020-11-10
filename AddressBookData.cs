90b2a1bc68d1b4ca9cbbfe171c1854aabb4c68e9
Ruthikreddy <ruthikreddy15@gmail.com>
2020-11-10 18:31:34 +0530

    [Ruthik] push for Master

---
+++ AddressBookData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLINQ
+{
+    public class AddressBookDataTable
+    {
+        // UC 1 Create a new DataTable
+        DataTable table = new DataTable("AddressBook");
+
+        /// <summary>
+        /// UC2
+        /// creating constructor
+        /// Adding columns by invoking constructor
+        /// </summary>
+        public AddressBookDataTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
+    }
+}
